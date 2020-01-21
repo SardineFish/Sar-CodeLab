56d25cab6271c18c9db2a4791c726e2b23430f01
SardineFish <Sardine@live.cn>
2020-01-21 19:29:38 +0800

    Update RLE to PNG renderer.

---
+++ RLE2PNG/color.js
@@ -0,0 +1,143 @@
+export class Color
+{
+    constructor(r, g, b, a = 1.0)
+    {
+        this.red = 255;
+        this.green = 255;
+        this.blue = 255;
+        this.alpha = 1.0;
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.alpha = a;
+    }
+    get hue()
+    {
+        const R = this.red / 255;
+        const G = this.green / 255;
+        const B = this.blue / 255;
+        const max = Math.max(R, G, B);
+        const min = Math.min(R, G, B);
+        let h;
+        if (max === min)
+            h = 0;
+        else if (max === R)
+            h = 60 * (0 + (G - B) / (max - min));
+        else if (max === G)
+            h = 60 * (2 + (B - R) / (max - min));
+        else if (max === B)
+            h = 60 * (4 + (R - G) / (max - min));
+        return h < 0 ? h + 360 : h;
+    }
+    get saturation()
+    {
+        const max = Math.max(this.red, this.green, this.blue) / 255;
+        const min = Math.min(this.red, this.blue, this.green) / 255;
+        if (max === 0)
+            return 0;
+        else if (min == 1)
+            return 0;
+        return (max - min) / (1 - Math.abs(max + min - 1));
+    }
+    get lightness()
+    {
+        const max = Math.max(this.red, this.green, this.blue) / 255;
+        const min = Math.min(this.red, this.blue, this.green) / 255;
+        return (max + min) / 2;
+    }
+    set hue(value)
+    {
+        this.setHSL(value, this.saturation, this.lightness);
+    }
+    set saturation(value)
+    {
+        this.setHSL(this.hue, value, this.lightness);
+    }
+    set lightness(value)
+    {
+        this.setHSL(this.hue, this.saturation, value);
+    }
+    static add(a, b)
+    {
+        const t = b.alpha;
+        return new Color((1 - t) * a.red + t * b.red, (1 - t) * a.green + t * b.green, (1 - t) * a.blue + t * b.blue, 1 - (1 - a.alpha) * (1 - b.alpha));
+    }
+    static blend(a, b, t)
+    {
+        return new Color((1 - t) * a.red + t * b.red, (1 - t) * a.green + t * b.green, (1 - t) * a.blue + t * b.blue, 1);
+    }
+    static fromHSL(h, s, l, alpha = 1)
+    {
+        return new Color(0, 0, 0, alpha).setHSL(h, s, l);
+    }
+    setHSL(h, s, l)
+    {
+        h = h < 0 ? h + 360 : h;
+        const chroma = (1 - Math.abs(2 * l - 1)) * s;
+        if (isNaN(h))
+        {
+            this.red = this.green = this.blue = 0;
+            return this;
+        }
+        h = h / 60;
+        const x = chroma * (1 - Math.abs(h % 2 - 1));
+        let color = [0, 0, 0];
+        if (0 <= h && h <= 1)
+            color = [chroma, x, 0];
+        else if (h <= 2)
+            color = [x, chroma, 0];
+        else if (h <= 3)
+            color = [0, chroma, x];
+        else if (h <= 4)
+            color = [0, x, chroma];
+        else if (h <= 5)
+            color = [x, 0, chroma];
+        else if (h <= 6)
+            color = [chroma, 0, x];
+        let m = l - chroma / 2;
+        this.red = Math.floor((color[0] + m) * 255);
+        this.green = Math.floor((color[1] + m) * 255);
+        this.blue = Math.floor((color[2] + m) * 255);
+        return this;
+    }
+    static fromString(str, alpha = 1)
+    {
+        str = str.replace(new RegExp(/\s/g), "");
+        var reg = new RegExp("#[0-9a-fA-F]{6}");
+        if (reg.test(str))
+        {
+            str = str.replace("#", "");
+            var strR = str.charAt(0) + str.charAt(1);
+            var strG = str.charAt(2) + str.charAt(3);
+            var strB = str.charAt(4) + str.charAt(5);
+            var r = parseInt(strR, 16);
+            var g = parseInt(strG, 16);
+            var b = parseInt(strB, 16);
+            return new Color(r, g, b, alpha);
+        }
+        reg = new RegExp("rgb\\(([0-9]+(\\.[0-9]+){0,1}),([0-9]+(\\.[0-9]+){0,1}),([0-9]+(\\.[0-9]+){0,1})\\)");
+        if (reg.test(str))
+        {
+            var colorArray = str.replace("rgb(", "").replace(")", "").split(",");
+            var r = parseInt(colorArray[0]);
+            var g = parseInt(colorArray[1]);
+            var b = parseInt(colorArray[2]);
+            var a = 1.00;
+            return new Color(r, g, b, a);
+        }
+        reg = new RegExp("rgba\\(([0-9]+(\\.[0-9]+){0,1}),([0-9]+(\\.[0-9]+){0,1}),([0-9]+(\\.[0-9]+){0,1}),([0-9]+(\\.[0-9]+){0,1})\\)");
+        if (reg.test(str))
+        {
+            var colorArray = str.replace("rgba(", "").replace(")", "").split(",");
+            var r = parseInt(colorArray[0]);
+            var g = parseInt(colorArray[1]);
+            var b = parseInt(colorArray[2]);
+            var a = parseFloat(colorArray[3]);
+            return new Color(r, g, b, a);
+        }
+    }
+    toString()
+    {
+        return `rgba(${this.red},${this.green},${this.blue},${this.alpha})`;
+    }
+}
--- RLE2PNG/script.js
+++ RLE2PNG/script.js
@@ -1,3 +1,5 @@
+import { Color } from "./color.js";
+
 /**
  * @type {HTMLCanvasElement}
  */
@@ -8,23 +10,42 @@ const canvas = document.querySelector("#canvas");
 const input = document.querySelector("#input");
 const ctx = canvas.getContext("2d");
 
+let aliveColor = new Color(255, 255, 255, 1);
+let deadColor = new Color(0, 0, 0, 1);
+let rleCode = document.querySelector("#input").value;
+
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+repaint();
 
 input.addEventListener("blur", () =>
 {
-    const text = input.value;
-    const data = parse(text);
+    rleCode = input.value;
+    repaint();
+});
+document.querySelector("#alive-color input").addEventListener("change", (e) =>
+{
+    aliveColor = Color.fromString(e.target.value);
+    repaint();
+});
+document.querySelector("#dead-color input").addEventListener("change", (e) =>
+{
+    deadColor = Color.fromString(e.target.value);
+    repaint();
+});
+function repaint()
+{
+    const data = parse(rleCode);
     canvas.width = data.width;
     canvas.height = data.height;
     ctx.putImageData(data, 0, 0);
-});
+}
 
 function parse(text = "")
 {
     let x = 0, y = 0, w = 0, h = 0;
     let runCount = 0;
-    let data;
+    let data = null;
     const pattern = {
         whiteSpace: /^(\s|\r\n|\r|\n)/,
         comment: /^#.*(\r\n|\r|\n)/,
@@ -68,11 +89,11 @@ function parse(text = "")
             text = text.substr(match[0].length);
             for (let i = 0; i < runCount; i++)
             {
-                idx = (y * w + (x + i)) * 4;
-                data.data[idx + 0] =
-                    data.data[idx + 1] =
-                    data.data[idx + 2] = 0;
-                data.data[idx + 3] = 255;
+                const idx = (y * w + (x + i)) * 4;
+                data.data[idx + 0] = deadColor.red;
+                data.data[idx + 1] = deadColor.green;
+                data.data[idx + 2] = deadColor.blue;
+                data.data[idx + 3] = Math.floor(deadColor.alpha * 255);
             }
             x += runCount;
             runCount = 1;
@@ -83,11 +104,11 @@ function parse(text = "")
             text = text.substr(match[0].length);
             for (let i = 0; i < runCount; i++)
             {
-                idx = (y * w + (x + i)) * 4;
-                data.data[idx + 0] =
-                    data.data[idx + 1] =
-                    data.data[idx + 2] =
-                    data.data[idx + 3] = 255;
+                const idx = (y * w + (x + i)) * 4;
+                data.data[idx + 0] = aliveColor.red;
+                data.data[idx + 1] = aliveColor.green;
+                data.data[idx + 2] = aliveColor.blue;
+                data.data[idx + 3] = Math.floor(aliveColor.alpha * 255);
             }
             x += runCount;
             runCount = 1;
@@ -100,11 +121,11 @@ function parse(text = "")
             {
                 for (; x < w; x++)
                 {
-                    idx = (y * w + x) * 4;
-                    data.data[idx + 0] =
-                        data.data[idx + 1] =
-                        data.data[idx + 2] = 0;
-                    data.data[idx + 3] = 255;
+                    const idx = (y * w + x) * 4;
+                    data.data[idx + 0] = deadColor.red;
+                    data.data[idx + 1] = deadColor.green;
+                    data.data[idx + 2] = deadColor.blue;
+                    data.data[idx + 3] = Math.floor(deadColor.alpha * 255);
                 }
                 x = 0;
                 y++;
@@ -117,14 +138,18 @@ function parse(text = "")
             text = text.substr(match[0].length);
             for (; x < w; x++)
             {
-                idx = (y * w + x) * 4;
-                data.data[idx + 0] =
-                    data.data[idx + 1] =
-                    data.data[idx + 2] = 0;
-                data.data[idx + 3] = 255;
+                const idx = (y * w + x) * 4;
+                data.data[idx + 0] = deadColor.red;
+                data.data[idx + 1] = deadColor.green;
+                data.data[idx + 2] = deadColor.blue;
+                data.data[idx + 3] = Math.floor(deadColor.alpha * 255);
             }
         }
         break;
     }
     return data;
 }
+
+
+
+
